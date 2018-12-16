a94d2d56d4bd5ad6f885242254a225864dd73565
topiaskarjalainen <topias.karjalainen@gmail.com>
2018-12-16 23:17:49 +0200

    fix

--- R/main.R
+++ R/main.R
@@ -1,14 +1,3 @@
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
 require(httr)
 require(jsonlite)
-site <- "https://api.spotify.com"
-
-id <- "f1e07008317e4742b4bf99e9c01499ba"
-sec <- "0036440a186845199ac0a2c3b4ac541b"
-
-setAuthentication(id, sec)
-getAccesstoken()
-getUserPlayslists("unski123")
